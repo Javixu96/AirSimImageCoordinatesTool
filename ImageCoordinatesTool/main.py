7795d9854bac7400ec5e8800ac4b03024476321e
Javixu96 <54887422+Javixu96@users.noreply.github.com>
2021-09-04 23:51:53 +0200

    Add files via upload

--- ImageCoordinatesTool/main.py
+++ ImageCoordinatesTool/main.py
@@ -11,13 +11,13 @@ def main():
     columnLat = 7
     columnHigh = 4
 
-    sheetTool.fillSheetWithCoordinates(sheet,columnPosX,Row,columnLat)
+    sheetTool.fillSheetWithCoordinates(sheet,columnPosX,inicialRow,columnLat)
 
     originFolder = "/home/example/pngFolder"
     destinationFolder = "/home/example/jpgFolder"
     
     imageTool.convertPngToJpg(originFolder,destinationFolder)
     
-    imageTool.insertCoordinatesInJpgExif(sheet,Row,columnLat,columnHigh,destinationFolder)
+    imageTool.insertCoordinatesInJpgExif(sheet,inicialRow,columnLat,columnHigh,destinationFolder)
   
 main()
